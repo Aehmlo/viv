048ff8759aa05c5b8566386d05c8560d538f6c94
Alex Hamilton <alex.hamilton@ou.edu>
2018-07-28 14:40:42 -0400

    Make TUI optional.
    This lays the foundation for plugging in a canvas-based API for a wasm
    version.
    
    Also use a random seed for the TUI.

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,63 @@
-extern crate termion;
 #[macro_use]
 extern crate viv;
-
-use termion::{clear, color, cursor, style, terminal_size};
-use viv::{Grid, Index};
+#[cfg(feature = "tui")]
+extern crate termion;
 
 use std::{
-    fmt, io::{self, prelude::*}, thread::sleep, time::Duration,
+    io::{self, prelude::*}, thread::sleep, time::Duration,
 };
 
-// TODO: Use background coloring and spaces instead, probably.
-const DEAD: &'static str = "◼️";
-const LIVE: &'static str = "◻️";
-const REFRESH_RATE: u64 = 1; // Hz
+#[cfg(feature = "tui")]
+mod term {
+    use std::fmt;
 
-struct Viewport {
-    width: u16,
-    height: u16,
-    grid: Option<Grid>,
-}
+    use termion::{cursor, terminal_size};
+    use viv::{Grid, Index};
 
-impl Viewport {
-    /// Constructs a new viewport showing the given number of rows and columns.
-    fn new(columns: u16, rows: u16) -> Self {
-        Self {
-            width: columns,
-            height: rows,
-            grid: None,
-        }
+    const DEAD: &'static str = "◼️";
+    const LIVE: &'static str = "◻️";
+    pub const REFRESH_RATE: u64 = 1; // Hz
+
+    pub struct Viewport {
+        pub grid: Grid,
     }
-    /// Returns the row-by-row output for this viewport.
-    fn rows(&self) -> impl IntoIterator<Item = String> {
-        let grid = self.grid.clone().unwrap();
-        let axes = ((self.width / 2) as isize, (self.height / 2) as isize);
-        let minima = (-axes.0, -axes.1);
-        let maxima = match (self.width, self.height) {
-            (x, y) if x % 2 == 0 && y % 2 == 0 => (axes.0 - 1, axes.1 - 1),
-            (x, _) if x % 2 == 0 => (axes.0 - 1, axes.1),
-            (_, y) if y % 2 == 0 => (axes.0, axes.1 - 1),
-            (_, _) => axes,
-        };
-        let mut rows = Vec::with_capacity(self.height as usize);
-        for y in minima.1..maxima.1 {
-            let mut s = String::with_capacity((self.width as usize) * 2);
-            for x in minima.0..maxima.0 {
-                let idx = index!(x, y);
-                s.push_str(if grid.is_living(&idx) { LIVE } else { DEAD });
+
+    impl Viewport {
+        /// Creates a new viewport.
+        pub fn new() -> Self {
+            Self {
+                grid: Grid::generate(),
             }
-            rows.push(s);
         }
-        rows
-    }
-}
 
-impl fmt::Display for Viewport {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if self.grid.is_none() {
-            write!(
-                f,
-                "{}",
-                DEAD.repeat(self.width as usize)
-                    .repeat(self.height as usize)
-            )
-        } else {
+        /// Returns the row-by-row output for this viewport.
+        pub fn rows(&self) -> impl IntoIterator<Item = String> {
+            let grid = self.grid.clone();
+            let (width, height) = terminal_size().unwrap_or((100, 40));
+            let axes = ((width / 2) as isize, (height / 2) as isize);
+            let minima = (-axes.0, -axes.1);
+            let maxima = match (width, height) {
+                (x, y) if x % 2 == 0 && y % 2 == 0 => (axes.0 - 1, axes.1 - 1),
+                (x, _) if x % 2 == 0 => (axes.0 - 1, axes.1),
+                (_, y) if y % 2 == 0 => (axes.0, axes.1 - 1),
+                (_, _) => axes,
+            };
+            let mut rows = Vec::with_capacity(height as usize);
+            for y in minima.1..maxima.1 {
+                let mut s = String::with_capacity((width as usize) * 2);
+                for x in minima.0..maxima.0 {
+                    let idx = index!(x, y);
+                    s.push_str(if grid.is_living(&idx) { LIVE } else { DEAD });
+                }
+                rows.push(s);
+            }
+            rows
+        }
+    }
+
+    impl fmt::Display for Viewport {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            print!("{}", cursor::Hide);
             for row in self.rows() {
                 writeln!(f, "{}", row)?;
             }
@@ -72,15 +67,14 @@ impl fmt::Display for Viewport {
 }
 
 fn main() {
-    let (width, height) = terminal_size().unwrap_or((100, 40));
-    let mut viewport = Viewport::new(width, height);
-    let grid = Grid::generate();
-    viewport.grid = Some(grid);
-    print!("{}", cursor::Hide);
-    loop {
-        print!("{}", viewport);
-        io::stdout().flush().expect("Failed to flush stdout.");
-        sleep(Duration::from_millis(1_000 / REFRESH_RATE));
-        viewport.grid = viewport.grid.map(|g| g.tick());
+    #[cfg(feature = "tui")]
+    {
+        let mut viewport = term::Viewport::new();
+        loop {
+            print!("{}", viewport);
+            io::stdout().flush().expect("Failed to flush stdout.");
+            sleep(Duration::from_millis(1_000 / term::REFRESH_RATE));
+            viewport.grid = viewport.grid.tick();
+        }
     }
 }
